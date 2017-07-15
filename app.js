36ed4512e3e8c78331d725006e2595e95f992a28
System <none>
2017-07-15 09:25:01 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -7,26 +7,8 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 //get call to return JSON that format natural and unix data
-app.get('/dataValues/:dataVal', function(req, res, next){
-    //console.log('url works');
-    var dateVal = req.params.dateVal;
-    var dataFormattingOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    };
-    
-    if(isNaN(dateVal)){
-        var naturalDate = new Date(dateVal);
-        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
-        var unixDate = new Date(dateVal).getTime()/1000;
-    }
-    else{
-        var unixDate = dateVal;
-        var naturalDate = new Date(dateVal *1000);
-        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
-    }
-    res.json({unix: unixDate, natural: naturalDate});
+app.get('/api/whoami', function(req, res, next){
+    console.log('url works');
 });
 
 app.listen(3000, function(){
