fe72fd3d2ec193b61d6fef54cd717b37c066a188
System <none>
2017-07-15 10:16:48 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
+var useragent = require("express-useragent");
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(useragent.express());
 //get call to return JSON that format natural and unix data
 var api = '/api/whoami';
 app.get(api, function(req, res, next){
   console.log('url works');
   var language = req.acceptsLanguages();
-  var software = req.get('User-Agent');
+  var software = 'OS: ' + req.useragent.os + ', Browser: ' + req.useragent.browser;
   var ipaddress = req.ip;
   res.json({'ipaddress ': ipaddress, 'language ': language[0], 'software': software}, );
 });
